6e9a8ce806d98d10e0074b166db6494fe19457fb
Shreya Mohan <shreyamohan.2nov@gmail.com>
2025-04-09 19:08:16 +0530

    RemoveK_Digits

---
+++ Stacks/RemoveK_Digits.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        string removeKdigits(string num, int k) {
+            stack<char> st;
+            string result;
+            
+            if (num.size() <= k) return "0";
+    
+            for (char digit : num) {
+                while (!st.empty() && k > 0 && st.top() > digit) {
+                    st.pop();
+                    k--;
+                }
+                st.push(digit);
+            }
+    
+            while (k > 0 && !st.empty()) {
+                st.pop();
+                k--;
+            }
+    
+            while (!st.empty()) {
+                result += st.top();
+                st.pop();
+            }
+    
+            reverse(result.begin(), result.end());
+    
+            int i = 0;
+            while (i < result.size() && result[i] == '0') {
+                i++;
+            }
+            result = result.substr(i);
+    
+            return result.empty() ? "0" : result;
+        }
+    };
+    int main()
+    {
+        string s;
+        cin >> s ;
+        int k ;
+        cin >> k;
+         
+    Solution sol;
+    cout << sol.removeKdigits(s, k) << endl;
+
+        return 0;
+    }
+
